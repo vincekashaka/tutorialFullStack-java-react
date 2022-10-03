0d2e4e97084fcc9bf4f0cbb13b217930f47b67ad
Vincent Kashaka <vincekashaka@gmail.com>
2022-10-03 11:39:27 +0200

    Axios http-common

--- tutorial-frontend/src/App.js
+++ tutorial-frontend/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/layout/Header';
+import TutorialDashboard from './components/TutorialDashboard';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Header />
+        <Routes>
+          <Route path='/tutorials' element={<TutorialDashboard />} />
+        </Routes>
       </div>
     </Router>
   );
